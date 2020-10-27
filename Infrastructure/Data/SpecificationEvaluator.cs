ddd271e43249deca8b5bb20657553050fb670dab
Charlene Gipulan <charlenegipulan@gmail.com>
2020-10-27 07:46:30 -0700

    create specification evaluator

---
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using Core.Entities;
+using Core.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, 
+            ISpecification<TEntity> spec)
+        {
+            var query = inputQuery;
+
+            if (spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }
+
+            //taking our include statements, and aggregate them and pass them into our query
+            //which is gonna be an iqueryable and we pass this to our method to query DB
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+
+            return query;
+        }
+    }
+}
