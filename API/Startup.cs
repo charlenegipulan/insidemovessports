040860bcbf0c6d1a46b532ec06a87224a29c8d25
Charlene Gipulan <charlenegipulan@gmail.com>
2020-11-03 22:51:03 -0800

    add swagger for documenting api

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -50,6 +51,10 @@ namespace API
                     return new BadRequestObjectResult(errorResponse);
                 };
             });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "InsideMoves API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -66,6 +71,9 @@ namespace API
             app.UseStaticFiles();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "InsideMoves Api V1"); });
 
             app.UseEndpoints(endpoints =>
             {
