4c4d7688c6c1253eeccd44008834c42813e5bfbd
Charlene Gipulan <charlenegipulan@gmail.com>
2020-11-04 22:28:12 -0800

    add sorting specification class

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -18,6 +18,16 @@ namespace Infrastructure.Data
                 query = query.Where(spec.Criteria);
             }
 
+            if (spec.OrderBy != null)
+            {
+                query = query.OrderBy(spec.OrderBy);
+            }
+
+            if (spec.OrderByDescending != null)
+            {
+                query = query.OrderByDescending(spec.OrderByDescending);
+            }
+
             //taking our include statements, and aggregate them and pass them into our query
             //which is gonna be an iqueryable and we pass this to our method to query DB
             query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
