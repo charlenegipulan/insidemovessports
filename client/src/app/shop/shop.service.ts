63151402ec0bbfc471a5d9b5b9503bf7398d6aeb
Charlene Gipulan <charlenegipulan@gmail.com>
2020-11-10 23:16:59 -0800

    initial set up of shop service

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IPagination } from '../shared/models/pagination';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopService {
+  baseUrl = 'https://localhost:5001/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+  getProducts() {
+    return this.http.get<IPagination>(this.baseUrl + 'products');
+  }
 }
