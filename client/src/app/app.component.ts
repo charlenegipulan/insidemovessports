780ed9695e3893a72af701875d0e0b53f89ab934
Charlene Gipulan <charlenegipulan@gmail.com>
2020-11-12 22:35:01 -0800

    create child component product item

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -11,18 +8,11 @@ import { IPagination } from './models/pagination';
 
 export class AppComponent implements OnInit{
   title = 'Inside Moves';
-  products: IProduct[];
 
-
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
 
   ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe(
-      (response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
+
   }
 }
